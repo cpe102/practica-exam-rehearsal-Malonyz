ea5910e6b57be8b102ca9c8447a7bdc1e0b681f3
Malonyz <mapmap1234@hotmail.com>
2020-01-10 15:24:44 +0700

    ex test 2

--- rehearsal_2.cpp
+++ rehearsal_2.cpp
@@ -1,1 +1,24 @@
+#include<iostream>
 
+using namespace std;
+ 
+ 
+ int main(){
+     float sum=0.0;
+     float x =0.0;
+    
+     cout << "Enter X: ";
+     cin >> x;
+     while (x != 0){
+         if (x > 0){
+             sum = sum + x;
+         }
+         cout << "Enter X: ";
+         cin >> x;
+
+     }
+     cout << "sum = " << sum;
+
+ }
+
+ 
