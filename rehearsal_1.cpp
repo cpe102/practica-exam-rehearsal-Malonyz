d6b6ad41ef0fdef816cd352d4f685ae6572b0c2a
Malonyz <mapmap1234@hotmail.com>
2020-01-10 15:04:05 +0700

    ex test

--- rehearsal_1.cpp
+++ rehearsal_1.cpp
@@ -1,1 +1,33 @@
+#include<iostream>
+
+using namespace std;
+ 
+ 
+ int main(){
+    string y;
+    float x;
+     cout << "What is your name? :";
+     cin >> y;
+     cout << "What is your GPA :";
+     cin >> x;
+     string a = " Inw Jrim Jrim!!!";
+     string b = "Try harder, ";
+     string u;
+
+     if (x >= 3.5){
+         u = y+a;
+
+     } else {
+         u = b+y;
+
+     }
+
+     cout << u;
+     
+     
+ }
+ 
+
+
+
 
